10985d77b39270f13f421b2ddc48766d8bd1ace2
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-01-29 14:38:46 +0100

    move foldernames to config.py

---
+++ config_template.py
@@ -0,0 +1,8 @@
+# config.py template
+# Add your respective folder names and save as config.py
+
+ROOT = "..."
+PATH_CODE = ROOT + "..."
+PATH_DATA = ROOT + "..."
+PATH_OUTPUT = PATH_DATA + "..."
+PATH_METADATA = PATH_DATA + "..."
