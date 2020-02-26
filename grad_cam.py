33e9716b64203722963104f874caa91906fec284
Breixo Soliño <breixo.solino@gmail.com>
2020-02-26 11:00:09 +0100

    Deleted guided backprop, unnecesary imports, moved to tf.keras

--- grad_cam.py
+++ grad_cam.py
@@ -1,13 +1,13 @@
-import sys
+#import sys
 import numpy as np
-import cv2
+#import cv2
 from matplotlib import pyplot as plt
-from keras import backend as K
-from keras.preprocessing import image
-from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from tensorflow.keras import backend as K
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
 
 import tensorflow as tf
-from tensorflow.python.framework import ops
+#from tensorflow.python.framework import ops
 
 # Define model here ---------------------------------------------------
 def build_model():
@@ -60,36 +60,7 @@ def normalize(x):
     return (x + 1e-10) / (K.sqrt(K.mean(K.square(x))) + 1e-10)
 
 
-def build_guided_model():
-    """Function returning modified model.
-    
-    Changes gradient function for all ReLu activations
-    according to Guided Backpropagation.
-    """
-    if "GuidedBackProp" not in ops._gradient_registry._registry:
-        @ops.RegisterGradient("GuidedBackProp")
-        def _GuidedBackProp(op, grad):
-            dtype = op.inputs[0].dtype
-            return grad * tf.cast(grad > 0., dtype) * \
-                   tf.cast(op.inputs[0] > 0., dtype)
-
-    g = tf.get_default_graph()
-    with g.gradient_override_map({'Relu': 'GuidedBackProp'}):
-        new_model = build_model()
-    return new_model
-
-
-def guided_backprop(input_model, images, layer_name):
-    """Guided Backpropagation method for visualizing input saliency."""
-    input_imgs = input_model.input
-    layer_output = input_model.get_layer(layer_name).output
-    grads = K.gradients(layer_output, input_imgs)[0]
-    backprop_fn = K.function([input_imgs, K.learning_phase()], [grads])
-    grads_val = backprop_fn([images, 0])[0]
-    return grads_val
-
-
-def grad_cam(input_model, image, n_class, layer_name):
+def grad_cam(input_model, input_image, n_class, layer_name):
     """GradCAM method for visualizing input saliency."""
     y_c = input_model.output[0, n_class]
     conv_output = input_model.get_layer(layer_name).output
@@ -98,20 +69,21 @@ def grad_cam(input_model, image, n_class, layer_name):
     # grads = normalize(grads)
     gradient_function = K.function([input_model.input], [conv_output, grads])
 
-    output, grads_val = gradient_function([image])
+    output, grads_val = gradient_function([input_image])
     output, grads_val = output[0, :], grads_val[0, :, :, :]
 
     weights = np.mean(grads_val, axis=(0, 1))
     cam = np.dot(output, weights)
 
     # Process CAM
-    cam = cv2.resize(cam, (W, H), cv2.INTER_LINEAR)
+#    cam = cv2.resize(cam, (W, H), cv2.INTER_LINEAR)
     cam = np.maximum(cam, 0)
     cam_max = cam.max() 
     if cam_max != 0: 
         cam = cam / cam_max
     return cam
-    
+
+
 def grad_cam_batch(input_model, images, classes, layer_name):
     """GradCAM method for visualizing input saliency.
     Same as grad_cam but processes multiple images in one run."""
@@ -129,7 +101,7 @@ def grad_cam_batch(input_model, images, classes, layer_name):
     for i in range(new_cams.shape[0]):
         cam_i = cams[i] - cams[i].mean()
         cam_i = (cam_i + 1e-10) / (np.linalg.norm(cam_i, 2) + 1e-10)
-        new_cams[i] = cv2.resize(cam_i, (H, W), cv2.INTER_LINEAR)
+#        new_cams[i] = cv2.resize(cam_i, (H, W), cv2.INTER_LINEAR)
         new_cams[i] = np.maximum(new_cams[i], 0)
         new_cams[i] = new_cams[i] / new_cams[i].max()
     
@@ -156,36 +128,20 @@ def compute_saliency(model, guided_model, img_path, layer_name='block5_conv3', n
     print("Explanation for '{}'".format(class_name))
     
     gradcam = grad_cam(model, preprocessed_input, n_class, layer_name)
-    gb = guided_backprop(guided_model, preprocessed_input, layer_name)
-    guided_gradcam = gb * gradcam[..., np.newaxis]
 
-    if save:
-        jetcam = cv2.applyColorMap(np.uint8(255 * gradcam), cv2.COLORMAP_JET)
-        jetcam = (np.float32(jetcam) + load_image(img_path, preprocess=False)) / 2
-        cv2.imwrite('gradcam.jpg', np.uint8(jetcam))
-        cv2.imwrite('guided_backprop.jpg', deprocess_image(gb[0]))
-        cv2.imwrite('guided_gradcam.jpg', deprocess_image(guided_gradcam[0]))
+#    if save:
+#        jetcam = cv2.applyColorMap(np.uint8(255 * gradcam), cv2.COLORMAP_JET)
+#        jetcam = (np.float32(jetcam) + load_image(img_path, preprocess=False)) / 2
+#        cv2.imwrite('gradcam.jpg', np.uint8(jetcam))
     
     if visualize:
         plt.figure(figsize=(15, 10))
-        plt.subplot(131)
         plt.title('GradCAM')
         plt.axis('off')
         plt.imshow(load_image(img_path, preprocess=False))
         plt.imshow(gradcam, cmap='jet', alpha=0.5)
 
-        plt.subplot(132)
-        plt.title('Guided Backprop')
-        plt.axis('off')
-        plt.imshow(np.flip(deprocess_image(gb[0]), -1))
-        
-        plt.subplot(133)
-        plt.title('Guided GradCAM')
-        plt.axis('off')
-        plt.imshow(np.flip(deprocess_image(guided_gradcam[0]), -1))
-        plt.show()
-        
-    return gradcam, gb, guided_gradcam
+    return gradcam
 
 
 # if __name__ == '__main__':
