81a034f101fed20cedad6b0a1c7c218099f8f89b
Breixo Soliño <breixo.solino@gmail.com>
2020-03-30 16:41:44 +0200

    Refactorization

--- visualization_utils.py
+++ visualization_utils.py
@@ -17,9 +17,8 @@ def visualize_gradcam(
         layer = None):
     fig = plt.figure(figsize = (16, 6.4))
     plt.axis('off')
-    ticks = [-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1]
-    n_plots = 2
-    n_halves = 0
+    n_rows = 1
+    n_cols = 2
     title = ""
     if label is not None:
         title += f", class {label}"
@@ -27,34 +26,20 @@ def visualize_gradcam(
         title += f", layer {layer}"
     plt.title(f'GradCAM{title}')
     if network_input is not None:
-        n_halves = 1
-        ax = fig.add_subplot(2, 1, 1)
+        n_rows += 1
+        ax = fig.add_subplot(n_rows, 1, 1)
         visualize_timeseries(network_input, ax = ax)
-        # fig.add_subplot(1, 2, 1)
         plt.axis('off')
-        # plt.imshow(np.transpose(network_input))
-        # plt.imshow(network_input.reshape(network_input.shape[0:2]))
-        # plt.imshow(np.repeat(network_input.reshape(network_input.shape[0:2]), 8, 0))
-        # plt.colorbar(ticks=ticks, orientation='horizontal')
         
     gradcam_floored = np.maximum(gradcam, 0)
 
-    # fig.add_subplot(n_halves + 1, n_plots, n_halves*n_plots +1)
-    ax = fig.add_subplot(n_plots, n_halves + 1, n_halves*n_plots +1)
+    ax = fig.add_subplot(n_rows, n_cols, n_rows*(n_cols-1) +1)
     plt.axis('off')
-    # plt.imshow(np.repeat(gradcam_floored, 16, 0))
     superpose_gradcam(gradcam_floored, network_input, ax = ax)
-    # plt.imshow(gradcam_floored)
-    # plt.colorbar(ticks=ticks, orientation='horizontal')
     
-    # fig.add_subplot(n_halves +1, n_plots, n_halves*n_plots +2)
-    ax = fig.add_subplot(n_plots, n_halves +1, n_halves*n_plots +2)
+    ax = fig.add_subplot(n_rows, n_cols, n_rows*(n_cols-1) +2)
     plt.axis('off')
-    # plt.imshow(np.repeat(gradcam, 16, 0))
     superpose_gradcam(gradcam, network_input, ax = ax)
-    # plt.imshow(gradcam)
-    # plt.colorbar(ticks=ticks, orientation='horizontal')
-    # fig.show()
 
 
 def visualize_timeseries(X_ts, title = None, ax = None):
@@ -92,15 +77,14 @@ def superpose_gradcam(
     n_ch, w_input = network_input.shape[0:2]
     
     bg_cmap = cm.get_cmap('inferno')
-    
     norm = cm.colors.Normalize(vmin = np.min(gradcam), vmax = np.max(gradcam))
     bg_colors = bg_cmap(norm(gradcam))
+    alpha = 0.75
     
     if ax is None:
         plt.style.use('ggplot')
         fig, ax = plt.subplots(figsize=(20,(1+0.7 *n_ch*2)))
     
-    print(gradcam.shape)
     w_gradcam = gradcam.shape[1]
     step = (w_input-1)/(w_gradcam-1) #TODO Is this the correct way to approach this?
     gradcam_bar = np.arange(w_gradcam) * step
@@ -111,7 +95,7 @@ def superpose_gradcam(
             width = step,
             bottom = -2*i-1,
             color = bg_colors[i,:],
-            alpha = 0.75,
+            alpha = alpha,
             edgecolor = None)
         ax.plot((network_input[i, :] - i*2), color="black")
         
@@ -122,4 +106,5 @@ def superpose_gradcam(
     ax.figure.colorbar(
         cm.ScalarMappable(norm=norm, cmap=bg_cmap),
         ax=ax,
-        orientation='horizontal')
+        orientation='horizontal',
+        alpha = alpha)
