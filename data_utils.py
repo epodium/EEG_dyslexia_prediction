fd0c0e0a84523ee3ee55824f709f90303ca3aa33
Breixo Soliño <breixo.solino@gmail.com>
2020-05-25 11:08:20 +0200

    Refactored and revised, added unroll dataset

--- data_utils.py
+++ data_utils.py
@@ -9,62 +9,7 @@ from dataset_generator import DataGenerator
 
 
 
-def collect_labels():
-    label_collection = []
-    label_counts = []
 
-    for filename in files_csv:
-        y_EEG = read_labels(filename, PATH_DATA_processed)
-        labels_unique = list(set(y_EEG))
-        label_collection.append(labels_unique)
-
-        # Count instances for each unique label
-        label_count = []
-        for label in labels_unique:
-            idx = np.where(np.array(y_EEG) == label)[0]
-            label_count.append(len(idx))
-        label_counts.append(label_count)
-
-
-def read_csv_labels(filename, PATH):
-    metadata = []
-    filename = os.path.join(PATH, filename)
-    with open(filename, 'r') as readFile:
-        reader = csv.reader(readFile, delimiter=',')
-        for row in reader:
-            if len(row) > 0:
-                # metadata.append(row)
-                metadata.append(''.join(row)) # TODO Delete, hotfix for csv issue
-    readFile.close()
-
-    return metadata
-
-
-def split_dataset(main_labels, split_ratio):
-    IDs_train = []
-    IDs_val = []
-    IDs_test = []
-
-    for label in list(set(main_labels)):
-        idx = np.where(np.array(main_labels) == label)[0]
-        N_label = len(idx)
-        print("Found", N_label, "datapoints for label", label)
-
-        N_train = int(split_ratio[0] * N_label)
-        N_val = int(split_ratio[1] * N_label)
-        N_test = N_label - N_train - N_val
-        print("Split dataset for label", label, "into train/val/test fractions:", N_train, N_val, N_test)
-
-        # Select training, validation, and test IDs:
-        trainIDs = np.random.choice(idx, N_train, replace=False)
-        valIDs = np.random.choice(list(set(idx) - set(trainIDs)), N_val, replace=False)
-        testIDs = list(set(idx) - set(trainIDs) - set(valIDs))
-
-        IDs_train.extend(list(trainIDs))
-        IDs_val.extend(list(valIDs))
-        IDs_test.extend(list(testIDs))
-
-    return IDs_train, IDs_val, IDs_test
 
 
 def prepare_set(indices, full_x_data, full_y_data):
@@ -85,18 +30,35 @@ def exchange_channels(data):
             data.shape[0], data.shape[2], data.shape[1]), order = 'F')
 
 
-def prepare_generators(path_data):
 
-    main_labels # list of all main labels.
-    label_dict #Dictionary for how to ouput the found labels to the model.
-    binarizer_dict #Dictionary for how to ouput the found labels to the model.
-    ignore_labels
+def unroll_generator(generator):
+    first_batch = True
+    for batch in generator:
+        if first_batch:
+            first_batch = False
+            x_set = batch[0]
+            y_set = batch[1]
+        else:
+            x_set = np.concatenate((x_set, batch[0]))
+            y_set = np.concatenate((y_set, batch[1]))
+    return x_set, y_set
 
 
+def prepare_generators(
+        path_data,
+        main_label_dict,
+        label_dict,
+        split_ratio,
+        ignore_labels = []):
 
     dirs = os.listdir(path_data)
     files_csv = fnmatch.filter(dirs, "*.csv")
 
+    main_labels = list_main_labels(path_data, files_csv, main_label_dict)
+
+    binarizer_dict  = binarize_labels(label_dict)
+
+    id_split = split_dataset(split_ratio, main_labels)
     generators = []
     for id_list in id_split:
         generator = DataGenerator(
@@ -107,15 +69,100 @@ def prepare_generators(path_data):
             ignore_labels = ignore_labels,
             path_data = path_data,
             filenames = [x[:-4] for x in files_csv],
-            data_path = path_data, # TODO Repeated?
+            data_path = path_data,
             to_fit=True,
-            n_average = 40,
-            batch_size = 32,
-            iter_per_epoch = 50,
+            n_average = 50,
+            batch_size = 10,
+            iter_per_epoch = 10,
             up_sampling = True,
             n_timepoints = 501,
             n_channels=30,
-            n_classes=1,
+            # n_classes=1,
             shuffle=True)
         generators.append(generator)
     return generators
+
+
+
+
+def read_labels(filename, PATH):
+    metadata = []
+    filename = os.path.join(PATH, filename)
+    with open(filename, 'r') as readFile:
+        reader = csv.reader(readFile, delimiter=',')
+        for row in reader:
+            if len(row) > 0:
+                # metadata.append(row)
+                metadata.append(''.join(row)) # TODO Delete, hotfix for csv issue
+    readFile.close()
+
+    return metadata
+
+
+def binarize_labels(label_dict):
+    values = [int(x) for x in label_dict.values()]
+    size = np.max(values)
+    binarizer_dict = dict()
+    for value in set(label_dict.values()):
+        bin_value = [0] * (size+1)
+        bin_value[int(value)] = 1
+        binarizer_dict[value] = bin_value
+    return binarizer_dict
+
+
+def list_main_labels(path_data, files_csv, main_label_dict):
+    label_collection = []
+
+    for filename in files_csv:
+        y_EEG = read_labels(filename, path_data)
+        labels_unique = list(set(y_EEG))
+        label_collection.append(labels_unique)
+
+
+    main_labels = [main_label_dict.get(x[0]) for x in label_collection]
+    return main_labels
+
+
+def read_csv_labels(filename, PATH):
+    metadata = []
+    filename = os.path.join(PATH, filename)
+    with open(filename, 'r') as readFile:
+        reader = csv.reader(readFile, delimiter=',')
+        for row in reader:
+            if len(row) > 0:
+                # metadata.append(row)
+                metadata.append(''.join(row)) # TODO Delete, hotfix for csv issue
+    readFile.close()
+
+    return metadata
+
+
+def split_dataset(split_ratio, main_labels):
+    # id_split = [[]]*3
+    ids_train = []
+    ids_val = []
+    ids_test = []
+
+    for label in list(set(main_labels)):
+        idx = np.where(np.array(main_labels) == label)[0]
+        N_label = len(idx)
+        print("Found", N_label, "datapoints for label", label)
+
+        N_train = int(split_ratio[0] * N_label)
+        N_val = int(split_ratio[1] * N_label)
+        N_test = N_label - N_train - N_val
+        print("Split dataset for label", label, "into train/val/test fractions:", N_train, N_val, N_test)
+
+        # Select training, validation, and test IDs:
+        trainIDs = np.random.choice(idx, N_train, replace=False)
+        valIDs = np.random.choice(list(set(idx) - set(trainIDs)), N_val, replace=False)
+        testIDs = list(set(idx) - set(trainIDs) - set(valIDs))
+
+        ids_train.extend(list(trainIDs))
+        ids_val.extend(list(valIDs))
+        ids_test.extend(list(testIDs))
+
+    np.random.shuffle(ids_train)
+    np.random.shuffle(ids_val)
+    np.random.shuffle(ids_test)
+    return ids_train, ids_val, ids_test
